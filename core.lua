04e45c76055f7df3665999bb2c0815b3a251db91
Ocularis <andreas.ocularis@gmail.com>
2010-06-02 23:51:36 +0200

    dry code is fun

--- core.lua
+++ core.lua
@@ -12,32 +12,35 @@ local mssg = "%s %s %s >>%s<<"
 local msgg = "%s %s %s"
 ------------------------------------------
 
+--Spam No More.
+
+local freq1 = 3
+local anno1 = time()
+
 
 ----registers events for the addon to function---
+
 local Phrot = CreateFrame("Frame")
 Phrot:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 print("|cffFF387AProtadin|r detected. |cffFF387APink Power|r |cffFF0000go!|r")
+
 Phrot:SetScript("OnEvent", function(self, event, ...)
 local event, source, dest, sourceName, destName, missType, id = select(2, ...), select(3, ...), select(4, ...), select(7, ...), select(12, ...)
 ------------------------------------------------------------------
-  local playerName = UnitName("player")--used for the buff check
-		----------message output detairmins channels to use------
+
+local playerName = UnitName("player")		--used for the buff check
+		
+----------message output detairmins channels to use------
+		
 function Phrot:SpewMessage(spew)
         if (GetNumRaidMembers() > 0) then
-           SendChatMessage(spew, "RAID")
+			SendChatMessage(spew, "RAID")
         elseif (GetNumPartyMembers() > 0) then
-           SendChatMessage(spew, "PARTY")
+			SendChatMessage(spew, "PARTY")
 		else
-	SendChatMessage(spew, "EMOTE")--
-        end     
-     end
-	 
-
-
-
-
-	-------------------------------------------------------------
-	 
+			SendChatMessage(spew, "EMOTE")--
+		end     
+    end
 		--Divine Protection--
    if event == "SPELL_AURA_APPLIED" and source == UnitGUID("player") then
 		id = select(9, ...)
@@ -46,81 +49,45 @@ function Phrot:SpewMessage(spew)
          Phrot:SpewMessage(spew)
       end
 	end
-	----------------------------------------------------------------------------
 		--Divine Sacrifice--
    if event == "SPELL_AURA_APPLIED" and source == UnitGUID("player") then
 		id = select(9, ...)
       if id == 64205 then
+		if ((anno1 + freq1) < time()) then
+			anno1 = time()
          local spew = format(msgg, GetSpellLink(id),"Used:", Sac_Say)
          Phrot:SpewMessage(spew)
+		end
       end
 	end
-	----------------------------------------------------------------------------	
-		--survival instincts--
-	--[[
-	if event == "SPELL_AURA_APPLIED" and source == UnitGUID("player") then
-		id = select(9, ...)
-      if id == 61336 then
-         local spew = format(msgg, GetSpellLink(id),"Used:", LastStand_Say)
-         Phrot:SpewMessage(spew)
-      end
-	end
-]]
 	
-	--fade announses--
+	--Announce Fades--
    if event == "SPELL_AURA_REMOVED" and source == UnitGUID("player") then
 		id = select(9, ...)
       if id == 498 or id == 64205 then
+		if ((anno1 + freq1) < time()) then
+			anno1 = time()
          local spew = format(msg, GetSpellLink(id)," ", "FADED")
          Phrot:SpewMessage(spew)
+		 end
       end 
 	  end
-	  
-	  ----------------------------------------------------------------
-	  
-	  			--challanging shout used announcments--
-		--[[
-	if event == "SPELL_CAST_SUCCESS" and source == UnitGUID("player") then
-      id = select(9, ...)
-      if id == 5209 then
-         local spew = format(msgg, GetSpellLink(id),"Used", ChallShout_Say)
-         Phrot:SpewMessage(spew)
-      end
-	end
-	  ]]
---------------------------------------------------	
 	
-	  --taunt, challanging shout and mocking blow resist announcements--
+	  --Missed Taunts?
 	  
 	if event == "SPELL_MISSED" and source == UnitGUID("player") then
 		missType = select(12, ...)
 		id = select(9, ...)
 	if id == 62124 then
+		if ((anno1 + freq1) < time()) then
+			anno1 = time()
     local spew = format(mssg, GetSpellLink(id), "FAILED on:", sourceName, missType)
 		Phrot:SpewMessage(spew)
+		end
    end
    end
-   
-			--------------------------------------------------------------------
 
-
---interupt test work in progress--
-
---[[
-    if event == "SPELL_INTERRUPT" and source == UnitGUID("player") then
-	local spew = format(mssg, sourceName, "II", missType, "Сбил нах!")
-		Phrot:SpewMessage(spew)
-	end]]
-----------------------------------------------------------------------------------------------------------
-			
-			--[[ to change annoouncments edit the text between "" e.g: "1", can be changed 
-			to "2", or if you would like to add new coment just type after the last comment on a new line "exsample text",
-			be sure to start with " end with ",						
-			--]]
-
-
-
-			---------------shield wall announcments---------------------------
+   --Divine Protection
    ShieldWallSay = {
 	"-50% damage",
 }
@@ -129,8 +96,7 @@ NumSaySW = table.getn( ShieldWallSay );
 SNumSW = math.random( NumSaySW );
 ShieldWall_Say = ShieldWallSay[SNumSW];
 
-
-			---------------4t10--------------------------------------------------------
+	--Divine Sacrifice
    SacSay = {
 	"-30% Raid Damage",
 }
@@ -138,27 +104,4 @@ ShieldWall_Say = ShieldWallSay[SNumSW];
 NumSaySW = table.getn( SacSay );
 SNumSW = math.random( NumSaySW );
 Sac_Say = SacSay[SNumSW];
-
----------------------------------last stand announcments------------------------------------
-	LastStandSay = {
-	"+45% HP",
-}
-
-NumSayLS = table.getn( LastStandSay );
-SNumLS = math.random( NumSayLS );
-LastStand_Say = LastStandSay[SNumLS];
-
-------------------------------------------------------------------------------------------
-
-
-
-							--[[challanging shout announcments--
-
-	ChallShoutSay = {
-	"Mass Taunt",
-}
-NumSayCS = table.getn( ChallShoutSay );
-SNumCS = math.random( NumSayCS );
-ChallShout_Say = ChallShoutSay[SNumCS];]]
-
 end)
